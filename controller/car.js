d9a7c4039c82a0105e71bc8284c15b536aa9c6c1
MurilooFss <ferreira.murilosilva@outlook.com>
2022-11-28 17:59:21 -0300

    Adicionado o crud básico conectado ao banco

---
+++ controller/car.js
@@ -0,0 +1,36 @@
+import { openDb } from "../dbConfig.js";
+
+export async function getCars(req, res) {
+    openDb().then(async (db) => {
+        db.all('select * from carros where finalizado is 0').then(cars => res.json(cars))
+
+    })
+}
+
+export async function getCar(req, res) {
+    openDb().then(async (db) => {
+        const id = req.body.id
+        db.all('select * from carros where id_carro is ?', [id]).then(cars => res.json(cars))
+
+    })
+}
+export async function insertCar(req, res) {
+    car = req.body
+    openDb().then(db => {
+        db.run('insert into carros(id_estacionamento, marca, modelo, cor, placa, tamanho, tipo, hora_entrada, finalizado) values(?,?,?,?,?,?,?,?,?)', [car.id_estacionamento, car.marca, car.modelo, car.cor, car.placa, car.tamanho, car.tipo, car.hora_entrada, car.finalizado])
+    })
+}
+export async function updateCar(req, res) {
+    let carUpdate = req.body
+    openDb().then(db => {
+        db.run('update carros set marca=?, modelo=?, cor=?, placa=?, tamanho=?, tipo=? where id_carro=?', [carUpdate.marca, carUpdate.modelo, carUpdate.cor, carUpdate.placa, carUpdate.tamanho, carUpdate.tipo, carUpdate.id_carro])
+    }).then(() => res.json('status ok'))
+
+}
+export async function deleteCar(req, res) {
+    let car = req.body
+    openDb().then(db => {
+        db.run('delete from carros where id_carro is ?', [car.id_carro])
+    }).then(() => res.json('status removido'))
+
+}
