99fa546586d64462c74c9949e14497d7db246331
MurilooFss <ferreira.murilosilva@outlook.com>
2022-12-01 17:56:13 -0300

    ajustado selecionar os carros ativos

--- controller/car.js
+++ controller/car.js
@@ -2,8 +2,8 @@ import { openDb } from "../dbConfig.js";
 
 export async function getCars(req, res) {
     openDb().then(async (db) => {
-        db.all('select * from carros where finalizado is 0').then(cars => res.json(cars))
-
+        const id_estacionamento = req.query.id_estacionamento
+        db.all('select * from carros where finalizado is 0 and id_estacionamento is ?', [id_estacionamento]).then(cars => res.json(cars))
     })
 }
 
@@ -11,7 +11,7 @@ export async function getCar(req, res) {
     openDb().then(async (db) => {
 
         const id = req.query.id_carro
-
+        console.log(id)
         db.all('select * from carros where id_carro is ?', [id]).then(cars => res.json(cars))
 
     })
@@ -20,6 +20,8 @@ export async function insertCar(req, res) {
     let car = req.body
     openDb().then(db => {
         db.run('insert into carros(id_estacionamento, marca, modelo, cor, placa, tamanho, tipo, hora_entrada, finalizado) values(?,?,?,?,?,?,?,?,?)', [car.id_estacionamento, car.marca, car.modelo, car.cor, car.placa, car.tamanho, car.tipo, car.hora_entrada, car.finalizado])
+    }).then(x => {
+        res.json('cadastrado')
     })
 }
 export async function updateCar(req, res) {
